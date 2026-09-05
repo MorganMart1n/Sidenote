6ac998f1be73d4feb039e610df7262e602b2945c
Morgan Martin <63368697+MorganMart1n@users.noreply.github.com>
2026-09-05 18:57:08 +0100

    fixxed bad push

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,16 +1,14 @@
-module com.example { 
-    requires transitive javafx.controls; 
-    requires javafx.fxml; 
-    requires transitive javafx.graphics; 
-    requires transitive javafx.base; 
+module com.example {
+    requires transitive javafx.controls;
+    requires javafx.fxml;
+    requires transitive javafx.graphics;
+    requires transitive javafx.base;
 
     requires transitive org.fxmisc.richtext;
     requires org.fxmisc.flowless;
     requires reactfx;
 
-    opens com.example to javafx.fxml; 
+    opens com.example to javafx.fxml;
 
-    opens com.example.css to javafx.graphics; 
-
-    exports com.example; 
+    exports com.example;
 }
